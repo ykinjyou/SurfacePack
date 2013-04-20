b5735d1d51c818ccff2bb39131bfd62cadac9aa4
Yk <Yk@macbookpro>
2013-04-21 01:38:00 +0900

    20130421 macbookpro Yk

--- YkSample_SurfaceView/src/yk/Sample/MainActivity.java
+++ YkSample_SurfaceView/src/yk/Sample/MainActivity.java
@@ -1,8 +1,12 @@
 package yk.Sample;
 
+import yk.Screen.Pack.BitmapObject;
 import yk.Screen.Pack.SurfaceOriginalDrawListener;
 import yk.Screen.Pack.SurfaceScreen;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,15 +18,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		Resources res = getResources();
+		Bitmap bm = BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+		
+		final BitmapObject bo = new BitmapObject(bm);
 		SurfaceScreen sf = new SurfaceScreen(this, 500);
 		sf.setOnDrawing(new SurfaceOriginalDrawListener() {
 			@Override
 			public void onDrawSurface(Canvas canvas) {
 				// 描画処理
 				canvas.drawColor(Color.BLUE);
-				Paint paint = new Paint();
-				paint.setColor(Color.BLACK);
-				canvas.drawText("TEST", 30, 30, paint);
+				
+				bo.draw(canvas);
 			}
 		});
 		
--- YkSample_SurfaceView/src/yk/Screen/Pack/BitmapObject.java
+++ YkSample_SurfaceView/src/yk/Screen/Pack/BitmapObject.java
@@ -1,30 +1,86 @@
 package yk.Screen.Pack;
 
+import java.util.ArrayList;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-
+import android.util.Log;
+/**
+ * Bitmap画像アニメーションする為のクラス
+ * @author Yk
+ *
+ */
 public class BitmapObject {
-	
+	/** LOG */
+	private final static String CLASS = "BitmapObject";
+	/** PAINT */
+	private Paint paint;
+	/** BITMAPLIST */
+	private ArrayList<Bitmap> bitMapList;
+	/** LoopFlg */
+	private boolean loop = false;
+	/** カウント */
 	private int cnt;
+	/** X座標 */
 	private float position_X;
+	/** Y座標 */
 	private float position_Y;
 	
+	/**
+	 * Contractor
+	 */
 	public BitmapObject() {
+		Log.d(CLASS, "BitmapObject()");
+		
 		position_X = 0;
 		position_Y = 0;
 		cnt = 0;
 	}
+	/**
+	 * Contractor
+	 * @param posX X初期位置
+	 * @param posY Y初期位置
+	 */
 	public BitmapObject(float posX, float posY) {
+		Log.d(CLASS, "BitmapObject(X,Y)");
+		
 		position_X = posX;
 		position_Y = posY;
 		cnt = 0;
 	}
+	/**
+	 * Contractor
+	 * @param bitmap BITMAP画像
+	 */
+	public BitmapObject(Bitmap bitmap) {
+		Log.d(CLASS, "BitmapObject(bitmap)");
+		
+		bitMapList = new ArrayList<Bitmap>();
+		bitMapList.add(bitmap);
+		position_X = 0;
+		position_Y = 0;
+		cnt = 0;
+	}
 	
-	public void drow(Canvas canvas) {
-		Paint paint = new Paint();
-		paint.setColor(Color.BLACK);
-		canvas.drawText("" + cnt, position_X, position_Y, paint);
+	/**
+	 * 描画処理
+	 * @param canvas
+	 */
+	public void draw(Canvas canvas) {
+		
+		paint = new Paint();
+		canvas.drawBitmap(bitMapList.get(cnt), position_X, position_Y, paint);
+		
+		if(bitMapList.size() >= cnt){
+			
+			if(loop == true){
+				cnt = 0;
+			}
+			
+		}else{
+			cnt++;
+		}
+		
 	}
 
 }
